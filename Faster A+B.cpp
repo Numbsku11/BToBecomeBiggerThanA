f87d433201eac95560c29c4ca4972a79960ef582
Numbsku11 <164810583+Numbsku11@users.noreply.github.com>
2024-05-14 18:26:37 +0100

    Update Faster A+B.cpp

--- Faster A+B.cpp
+++ Faster A+B.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <cmath>
 
+// I couldn't tell you why these are global
 int A1 = 1;
 int A2 = 0;
 
@@ -18,66 +19,61 @@ double BResults = 0;
 int FileWrite()
 {
 
-  std::ofstream outputFile("results.txt", std::ios::app);
+std::ofstream outputFile("results.txt", std::ios::app);
+// Check if the file is open
+if (outputFile.is_open())
+{       
+// Write the results to the file
+outputFile << "Test Number: " << TestNumber << "\n\n"
+           << " A1= " << A1 << "\n\n" << " A2= " << A2 << "\n\n" << " A Results are: " << AResults
+           << "\n\n" << " B1= " << B1 << "\n\n" << " B2= " << B2 << "\n\n" << " B Results are: " << BResults << "\n\n";
 
-    // Check if the file is open
-    if (outputFile.is_open())
-    {
-        // Write the results to the file
-        outputFile << "Test Number: " << TestNumber << "\n\n"
-                   << " A1= " << A1 << "\n\n" << " A2= " << A2 << "\n\n" << " A Results are: " << AResults
-                   << "\n\n" << " B1= " << B1 << "\n\n" << " B2= " << B2 << "\n\n" << " B Results are: " << BResults << "\n\n";
+// Close the file
+outputFile.close();
 
-        // Close the file
-        outputFile.close();
-    }
+}
+else
+{
+std::cerr << "Unable to open the file for writing." << std::endl;
+}
 
-    else
-    {
-        std::cerr << "Unable to open the file for writing." << std::endl;
-    }
 }
 
 
 int main()
 {
 
-        std::ofstream outputFile("results.txt", std::ios::app);
+  std::ofstream outputFile("results.txt", std::ios::app);
 
-            if (outputFile.is_open())
-            {
-            outputFile << "Test for when or if B would be come bigger than A when made to a power" << "\n\n\n\n";
-
-            outputFile.close();
-            }
-            else
-            {
-                std::cerr << "Can't open file to write to" << std::endl;
-            }
-    while (TRUE)
-    {
+  if (outputFile.is_open())
+  {
+  outputFile << "Test for when or if B would be come bigger than A when made to a power\n\n\n\n";
+  outputFile.close();
+  }
+  else
+  {
+  std::cerr << "Can't open file to write to" << std::endl;
+  }
+  
+  while (TRUE)
+  {
 
     AResults = pow(A1,A2);
     BResults = pow(B1,B2);
-
-    std::cout << AResults << "\n\n" << BResults;
-
+    std::cout << AResults << "\n\n" << BResults; 
     FileWrite();
-
+    
     if(AResults > BResults)
-        {
-        A1++;
-        A2++;
-        B1++;
-        B2++;
-        TestNumber++;
-        }
-        else
-        {
-            break;
-        }
-
-        }
-
+    {
+      A1++;
+      A2++;
+      B1++;
+      B2++;
+      TestNumber++;
+    }
+    else
+    {
+    break;
+    }
+  }
 }
-

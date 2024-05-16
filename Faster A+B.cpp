8da7b8d349b3b6eed616db9b0215d3d301407dd5
Numbsku11 <mswift823@gmail.com>
2024-05-16 14:46:11 +0100

    Cleanup and reformatted

--- Faster A+B.cpp
+++ Faster A+B.cpp
@@ -1,79 +1,76 @@
-#include <iostream>
-#include <fstream>
-#include <windows.h>
-#include <cmath>
+#include "headers.h"
 
-// I couldn't tell you why these are global
 int A1 = 1;
-int A2 = 0;
-
-int B1 = 0;
+int A2;
+int B1; 
 int B2 = 1;
+// A: 1^0 B: 0^1 
 
 int TestNumber = 1;
-int ExitProgram = 0;
-
-double AResults = 0;
-double BResults = 0;
+double AResults;
+double BResults; 
 
 int FileWrite()
 {
 
-std::ofstream outputFile("results.txt", std::ios::app);
-// Check if the file is open
-if (outputFile.is_open())
-{       
-// Write the results to the file
-outputFile << "Test Number: " << TestNumber << "\n\n"
-           << " A1= " << A1 << "\n\n" << " A2= " << A2 << "\n\n" << " A Results are: " << AResults
-           << "\n\n" << " B1= " << B1 << "\n\n" << " B2= " << B2 << "\n\n" << " B Results are: " << BResults << "\n\n";
+    std::ofstream outputFile("results.txt", std::ios::app); 
+      // Check if the file is open
+    if (outputFile.is_open())
+    {    
+    
+        // Write the results to the file
+        outputFile << "Test Number: " << TestNumber << "\n\n"
+        << " A1= " << A1 << "\n\n" << " A2= " << A2 << "\n\n" << " A Results are: " << AResults
+        << "\n\n" << " B1= " << B1 << "\n\n" << " B2= " << B2 << "\n\n" << " B Results are: " << BResults << "\n\n";
+        outputFile.close();
+    }
 
-// Close the file
-outputFile.close();
+    else
+    {
+        std::cerr << "Unable to open the file for writing." << std::endl;
+        // if you get this, something fucked up
+    }
 
+    return 0;
 }
-else
-{
-std::cerr << "Unable to open the file for writing." << std::endl;
-}
-
-}
-
 
 int main()
 {
+    // std::remove("results.txt"); use this if you need to iterate quickly
+    std::ofstream outputFile("results.txt", std::ios::app);
 
-  std::ofstream outputFile("results.txt", std::ios::app);
-
-  if (outputFile.is_open())
-  {
-  outputFile << "Test for when or if B would be come bigger than A when made to a power\n\n\n\n";
-  outputFile.close();
-  }
-  else
-  {
-  std::cerr << "Can't open file to write to" << std::endl;
-  }
-  
-  while (TRUE)
-  {
-
-    AResults = pow(A1,A2);
-    BResults = pow(B1,B2);
-    std::cout << AResults << "\n\n" << BResults; 
-    FileWrite();
-    
-    if(AResults > BResults)
+    if (outputFile.is_open())
     {
-      A1++;
-      A2++;
-      B1++;
-      B2++;
-      TestNumber++;
+    outputFile << "Test for when or if B would be come bigger than A when made to a power\n\n\n\n";
+    outputFile.close();
+    // header infomation
     }
     else
     {
-    break;
+    std::cerr << "Can't open file to write to" << std::endl;
     }
-  }
+
+    while (TRUE)
+    {
+        // calculates the powers into a whole number 
+        AResults = pow(A1,A2);
+        BResults = pow(B1,B2);
+        FileWrite();    
+
+        if(AResults > BResults) // adds 1 and runs the test again
+        {
+            A1++;
+            A2++;
+            B1++;
+            B2++;
+            TestNumber++;
+        }
+        else
+        {
+        break;
+        }
+    }
+    std::cout <<"Press any key to close the program";
+    std::cin.get();
+    return 0;
 }
